5e6caf83627a04afc2106e54d8284c00855489f1
Mikelish <mikelis@sponsorking.lv>
2018-03-06 16:56:06 +0200

    Laravel Mix up and running + scripts and stylesheets included

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const { mix } = require('laravel-mix');
+let mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -7,9 +7,25 @@ const { mix } = require('laravel-mix');
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
  | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
+ | file for your application, as well as bundling up your JS files.
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.scripts([
+
+     'resources/assets/js/bootstrap.js',
+     'resources/assets/js/jquery.js',
+     'resources/assets/js/metisMenu.js',
+     'resources/assets/js/sb-admin-2.js'
+
+], path.normalize('./public/js/libs.js'));
+
+mix.styles([
+
+     'resources/assets/css/blog-post.css',
+     'resources/assets/css/bootstrap.css',
+     'resources/assets/css/font-awesome.css',
+     'resources/assets/css/metisMenu.css',
+     'resources/assets/css/sb-admin-2.css'
+
+], path.normalize('./public/css/libs.css'));
